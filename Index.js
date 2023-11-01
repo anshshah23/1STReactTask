8fec459e7ae2055bc95c3a4d06f86c377939be6d
Ansh Shah <122973942+anshshah23@users.noreply.github.com>
2023-11-01 20:42:07 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const Home = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'centre',
+		alignItems: 'centre',
+		height: '100vh'
+	}}
+	>
+	<h1>Welcome to Ansh Shah's server</h1>
+	</div>
+);
+};
+
+export default Home;
